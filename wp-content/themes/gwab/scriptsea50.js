cf7a528657b66f3bbaab083615e1f65bf59416be
Stung <stephen.tung@candrholdings.com>
2017-04-22 10:45:19 +0800

    First commit!

---
+++ wp-content/themes/gwab/scriptsea50.js
@@ -0,0 +1,13 @@
+jQuery(document).ready(function($){
+  $(document).bind('em_maps_location_hook', function(e, map, infowindow, marker) {
+    // todo
+  });
+  
+  $(document).bind('em_maps_locations_hook', function(e, map, infowindow, markers) {
+    // todo
+  //  var icon = new google.maps.MarkerImage("http://www.myiconfinder.com/uploads/iconsets/256-256-a5485b563efc4511e0cd8bd04ad0fe9e.png");
+  //  for (var i = 0; i < markers.length; i++) {
+	// 	  markers[i].setOptions({"icon": icon});
+	//  }
+  });
+});
